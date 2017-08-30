6f66fdf1626f61f859b3842718197df1652dc32f
weitao <weitaoee@gmail.com>
2017-08-30 12:51:05 +0800

    add

--- tcp_server.c
+++ tcp_server.c
@@ -2,14 +2,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <errno.h>
 
 int main(void)
 {
-	int link_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (link_socket < 0)
+	int link_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (link_sock < 0)
 	{
 		print("create socket error\n");
 		return -1;
 	}
+
+	struct sockaddr_in sock_addr;
+	sock_addr.sin_family = AF_INET;
+	sock_addr.sin_port = htons(8888);
+	sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	bzero(&(sock_addr.sin_zero), 8);
 }
