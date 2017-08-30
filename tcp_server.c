c05cd1eac38a13caff29b09b3b9d543297e0b292
weitao <weitaoee@gmail.com>
2017-08-30 18:35:08 +0800

    add

--- tcp_server.c
+++ tcp_server.c
@@ -6,12 +6,15 @@
 #include <string.h>
 #include <errno.h>
 
+#define MAX_LINK_COUNT 16
+
 int main(void)
 {
-	int link_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (link_sock < 0)
+	int err = 0;
+	int listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listen_sock < 0)
 	{
-		print("create socket error\n");
+		printf("create socket error!\n");
 		return -1;
 	}
 
@@ -20,4 +23,29 @@ int main(void)
 	sock_addr.sin_port = htons(8888);
 	sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	bzero(&(sock_addr.sin_zero), 8);
+
+	if (bind(listen_sock, (struct sockaddr *)&sock_addr, sizeof(struct sockaddr)) != 0)
+	{
+		printf("bind socket error!\n");
+		return -1;
+	}
+
+	if (listen(listen_sock, MAX_LINK_COUNT) != 0)
+	{
+		printf("listen socket error!\n");
+		return -1;
+	}
+
+	while (1)
+	{
+		struct sockaddr_in client_addr;
+		socklen_t cli_len = sizeof(struct sockaddr_in);
+		int conn_sock = accept(listen_sock, cli_len);
+		if (conn_sock < 0 )
+		{
+			printf("accept socket error!\n");
+		}
+	}
+
+	exit(0);
 }
